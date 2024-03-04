6123d754dc8864a7d16eb1fc57da6a58d65f4455
Kamil Braun <kbraun@scylladb.com>
2024-03-04 21:07:16 +0100

    repair trace, disable consistent-topo

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 export SCYLLA_HOME={{scylla_home}}
 set -x
-{{scylla_bin}}/scylla --smp=2 --developer-mode=true
+{{scylla_bin}}/scylla --smp=2 --developer-mode=true --logger-log-level "repair=trace"
